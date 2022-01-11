86a5784ef38d6aacd7dbbb2e8f1be6fca27d74fd
dec03 <93152845+dec03@users.noreply.github.com>
2022-01-11 17:05:16 +0000

    i made an outline of the menu

--- menu.bat
+++ menu.bat
@@ -1,1 +1,28 @@
-test
+#!/bin/bash
+
+main () {
+echo "Option 1: Date/time"
+echo "Option 2: Calendar"
+echo "Option 3: Delete"
+echo "Option 4: System configuration"
+echo "Option 5: Exit"	
+read choice
+if [ $choice == 1 ]; then
+date_time
+fi
+}
+
+
+
+date_time () {
+date
+read date_and_time
+echo "$date_and_time"
+main
+}
+
+
+
+
+
+main
