c1261d1cf35d5d2c0934d73bd0c34f41e4b79dca
dec03 <93152845+dec03@users.noreply.github.com>
2022-01-11 18:47:54 +0000

    Update menu.bat

--- menu.bat
+++ menu.bat
@@ -1,6 +1,11 @@
 #!/bin/bash
 #this is the main function that allows the user to navigate to the different parts of the code
 main () {
+echo
+echo
+echo "WELCOME TO THE MAIN MENU"
+echo
+echo
 echo "Option 1: Date/time"
 echo "Option 2: Calendar"
 echo "Option 3: Delete"
@@ -8,11 +13,28 @@ echo "Option 4: System configuration"
 echo "Option 5: Exit"	
 #this take the input from the user and assigns it to the variable choice
 read choice
+
+
+
+
 #If statement will go to the date_time function if user input is 1
 if [ $choice == 1 ]; then
 #calls the date_time function
 date_time
 fi
+
+
+
+
+
+if [ $choice == 4 ]; then
+#this calls the system configuration menu
+system_config_menu
+fi
+
+
+
+
 #this kills the program
 if [ $choice == 5 ]; then
 kill
@@ -30,6 +52,31 @@ echo
 #this goes back to the main function
 main
 }
+
+
+#system config menu
+system_config_menu () {
+echo
+echo
+echo "WELCOME TO THE SYSTEM CONFIGURATION INFORMATION MENU"
+echo
+echo
+echo "Option 1: Operating System Type"
+echo "Option 2: CPU"
+echo "Option 3: Memory"
+echo "Option 4: Hard Disk"
+echo "Option 5: File System"
+echo "Option 6: Main menu"
+#gets input from user and assigns it to this variable
+read choice_two
+if [ $choice_two == 6 ]; then
+#this takes user back to the menu
+main	
+fi
+}
+
+
+
 
 
 main
